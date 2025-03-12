d5cd70a663b750fd069e986b6a3ab290659db33b
AntSib <antonsibilev2005@gmail.com>
2025-03-12 21:20:33 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            'style-loader',
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            'css-loader',
+            // Loads a SASS/SCSS file and compiles it to CSS
+            'sass-loader',
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer
+                ]
+              }
+            }
+          }
+        ]
+      },
+      { // Loader for webpack to process CSS
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+}
